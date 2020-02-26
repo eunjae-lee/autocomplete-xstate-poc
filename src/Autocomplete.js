66ceb33ae5861a007b9c3aa3240759f87cddf967
eunjae-lee <karis612@gmail.com>
2020-02-26 10:58:14 +0100

    add dropdown

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -2,9 +2,10 @@ import React from "react";
 import { useMachine } from "@xstate/react";
 import { Machine, assign } from "xstate";
 import { getSources } from "./algolia";
+import "./Autocomplete.css";
 
-const autocompleteMachine = Machine({
-  id: "autocomplete",
+const searchMachine = Machine({
+  id: "search",
   initial: "idle",
   context: {
     hits: []
@@ -21,42 +22,82 @@ const autocompleteMachine = Machine({
       }
     },
     success: {
-      entry: ["setHits"],
+      entry: ["setHits", "openDropdownIfHitsExist"],
       on: { INPUT: "searching" }
     }
   }
 });
 
+const dropdownMachine = Machine({
+  id: "dropdown",
+  initial: "closed",
+  states: {
+    closed: {
+      on: { TOGGLE: "opened", OPEN: "opened" }
+    },
+    opened: {
+      on: { TOGGLE: "closed", CLOSE: "closed" }
+    }
+  }
+});
+
 export default () => {
-  const [state, send] = useMachine(autocompleteMachine, {
+  const [searchState, sendToSearch] = useMachine(searchMachine, {
     actions: {
       search: (_, { data: { query } }) => {
         getSources()[0]
           .getSuggestions({ query })
           .then(hits => {
-            send({ type: "FETCHED", data: { hits } });
+            sendToSearch({ type: "FETCHED", data: { hits } });
           });
       },
       setHits: assign({
         hits: (_, { data }) => data.hits
-      })
+      }),
+      openDropdownIfHitsExist: ({ hits }) => {
+        if ((hits || []).length > 0) {
+          sendToDropdown({ type: "OPEN" });
+        }
+      }
     }
   });
+  const [dropdownState, sendToDropdown] = useMachine(dropdownMachine);
+
   const onInput = event => {
     const query = event.currentTarget.value;
-    send({ type: "INPUT", data: { query } });
+    sendToSearch({ type: "INPUT", data: { query } });
+  };
+
+  const onFocus = () => {
+    if ((searchState.context.hits || []).length > 0) {
+      sendToDropdown({ type: "OPEN" });
+    }
+  };
+
+  const onBlur = () => {
+    sendToDropdown({ type: "CLOSE" });
   };
 
   return (
     <div>
-      <input type="search" onInput={onInput} />
-      {(state.context.hits || []).map((hit, index) => {
-        return (
-          <div key={index}>
-            <p>{hit.name}</p>
-          </div>
-        );
-      })}
+      <input
+        type="search"
+        onInput={onInput}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+      {dropdownState.value === "opened" && (
+        <ul className="dropdown">
+          {(searchState.context.hits || []).map((hit, index) => {
+            return (
+              <div key={index}>
+                <p>{hit.name}</p>
+              </div>
+            );
+          })}
+        </ul>
+      )}
+      {/* <pre>{JSON.stringify(highlightState, null, 2)}</pre> */}
     </div>
   );
 };
